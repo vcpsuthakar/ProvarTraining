d0e56a7abd0c275c15591cadcae3d41e6e922ba2
112573 <112573@INDSNOIB61YXC2.AMERICAS.GLOBAL.NTTDATA.COM>
2019-04-13 10:50:46 +0530

    Message- Another change

---
+++ src/pageobjects/PageObject_Training.java
@@ -0,0 +1,21 @@
+package pageobjects;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.provar.core.testapi.annotations.*;
+
+@Page( title="PageObject_Training"                                
+     , summary=""
+     , relativeUrl=""
+     , connection="AdminConnection"
+     )             
+public class PageObject_Training {
+
+	@TextType()
+	@FindBy(xpath = "//label[normalize-space(.)='Account Name*']/following-sibling::input")
+	public WebElement accountName;
+			
+}
